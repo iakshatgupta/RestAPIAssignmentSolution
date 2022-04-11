a602930c8b7d5a1459419c8ee06866b3033fbd96
iakshatgupta <95222515+iakshatgupta@users.noreply.github.com>
2022-04-11 08:36:13 +0530

    Delete Employee Management Rest Api based Web app/main directory

--- Employee Management Rest Api based Web app/main/java/com/greatlearning/employeemanagementsystems/dao/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.employeemanagementsystems.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.employeemanagementsystems.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
